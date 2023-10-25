3390ac6c37c5d5867fd4c9d0e8552966b135303b
Mario <mario.espada77@gmail.com>
2023-10-25 02:49:11 +0200

    Add linter and github workflows for test and linter

--- internal/infrastructure/bootstrap/server/server.go
+++ internal/infrastructure/bootstrap/server/server.go
@@ -7,19 +7,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type server struct {
+type Server struct {
 	config *Config
 	engine *gin.Engine
 }
 
-func NewServer(config *Config) *server {
-	return &server{
+func NewServer(config *Config) *Server {
+	return &Server{
 		config: config,
 		engine: gin.Default(),
 	}
 }
 
-func (s *server) generateConnectionString() string {
+func (s *Server) generateConnectionString() string {
 	if s.config.Port == "" {
 		s.config.Port = "8080"
 	}
@@ -28,7 +28,7 @@ func (s *server) generateConnectionString() string {
 }
 
 // Run starts the server and listens for incoming requests on the specified address.
-func (s *server) Run() error {
+func (s *Server) Run() error {
 	corsConfig := s.corsConfig()
 	s.engine.Use(corsConfig)
 
@@ -37,7 +37,7 @@ func (s *server) Run() error {
 	return s.engine.Run(s.generateConnectionString())
 }
 
-func (s *server) corsConfig() gin.HandlerFunc {
+func (s *Server) corsConfig() gin.HandlerFunc {
 	return cors.New(cors.Config{
 		AllowAllOrigins: false,
 		AllowOrigins: []string{
@@ -49,7 +49,7 @@ func (s *server) corsConfig() gin.HandlerFunc {
 	})
 }
 
-func (s *server) healthCheck() {
+func (s *Server) healthCheck() {
 	s.engine.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
