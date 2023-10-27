c6663bcdd573de8ac37293e5f198ed3fbe6b4370
Mario <mario.espada77@gmail.com>
2023-10-27 02:55:45 +0200

    Add user Repositoy

---
+++ internal/infrastructure/db/connection.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func NewDatabaseConnection() (*gorm.DB, error) {
+	dbUser := os.Getenv("MYSQL_USER")
+	dbPass := os.Getenv("MYSQL_PASSWORD")
+	dbHost := os.Getenv("MYSQL_HOST")
+	dbPort := os.Getenv("MYSQL_PORT")
+	dbName := os.Getenv("MYSQL_DATABASE")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPass, dbHost, dbPort, dbName)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to database: %w", err)
+	}
+
+	return db, nil
+}
---
+++ internal/infrastructure/repositories/user/repository.go
@@ -0,0 +1,48 @@
+package repositories
+
+import (
+	"errors"
+
+	"github.com/bypepe77/Twitter-Clone-for-Golang/internal/domain/user"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{
+		db: db,
+	}
+}
+
+// CreateUser creates a new user in the database.
+func (r *userRepository) CreateUser(user *user.User) error {
+	dbUser := UserDBModel{
+		Username: user.Username,
+		Password: user.Password,
+	}
+
+	return r.db.Create(&dbUser).Error
+}
+
+// GetUserByID returns a user from the database by their usernme, if the user does not exist it returns nil.
+// If there is an error it returns the error.
+func (r *userRepository) GetUserByUsername(username string) (*user.User, error) {
+	var user UserDBModel
+
+	err := r.db.Where("username = ?", username).First(&user).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user.toUserDomainModel(), nil
+}
